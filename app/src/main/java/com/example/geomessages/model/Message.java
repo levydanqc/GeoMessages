8e88cb4d41f5975f77027969c253729c6941275d
Dan Levy <danlevy.ca@icloud.com>
2022-04-04 13:25:32 -0400

    Ajout du model de message.

---
+++ app/src/main/java/com/example/geomessages/model/Message.java
@@ -0,0 +1,74 @@
+package com.example.geomessages.model;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "messages_table")
+public class Message {
+
+    private Boolean isActive;
+    private String firstname;
+    private String lastname;
+    @PrimaryKey
+    private String picture;
+    private String latitude;
+    private String longitude;
+    private String message;
+
+    public Boolean getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(Boolean isActive) {
+        this.isActive = isActive;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
