2471760c1b415be4b34159aedffaf01723b51d34
Dan Levy <danlevy.ca@icloud.com>
2022-04-04 13:26:00 -0400

    Ajout de la requete des messages.

--- app/src/main/java/com/example/geomessages/ui/liste/ListeFragment.java
+++ app/src/main/java/com/example/geomessages/ui/liste/ListeFragment.java
@@ -1,6 +1,7 @@
 package com.example.geomessages.ui.liste;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,7 +11,14 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.geomessages.databinding.FragmentListeBinding;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class ListeFragment extends Fragment {
 
@@ -26,6 +34,25 @@ public class ListeFragment extends Fragment {
 
         final TextView textView = binding.textListe;
         listeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        RequestQueue queue = Volley.newRequestQueue(requireContext());
+        String url = "https://onoup.site/data.json";
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest
+                (Request.Method.GET, url, null, response ->
+                {
+                    for (int i = 0; i < response.length(); i++) {
+                        try {
+                            JSONObject jsonObject = response.getJSONObject(i);
+                            Log.d("TAG", "response: " + jsonObject.getString("picture"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                        error -> Log.d("TAG", "error: " + error));
+        queue.add(jsonArrayRequest);
+
 
         return root;
     }
